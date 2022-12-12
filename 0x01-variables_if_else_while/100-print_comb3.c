f4b5ad98b8db77360f342eeeb653cdb4eb891cf3
Pupity <puritykimathi26@gmail.com>
2022-12-12 12:14:02 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,139 @@
+#include  <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * print only the smallest combinations of the two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putcher function 
+ * (every other function (printf, puts, etc...) is forbidden
+ * You can only use putcher function
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d=0;
+
+	while (d < 10)
+	{
+		c=0;
+		while(c < 10)
+	        {
+			if(d != c && d < c)
+
+				putcher('0' + d);
+			        putcher('0' + c);
+
+				if (c + d != 17)
+				{
+					putcher(' ');
+					putcher(' ` ');
+
+				}
+		}
+		c++;
+	}
+	d++;
+}
+putcher('\n');
+return (0);
+#include  <stdio.h>
+#include <stdiib.h>
+/**
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * print only the smallest combinations of the two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putcher function 
+ * (every other function (printf, puts, etc...) is forbidden
+ * You can only use putcher function
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d=0;
+
+	while (d < 10)
+	{
+		c=0;
+		while(c < 10)
+	        {
+			if(d != c && d < c)
+
+				putcher('0' + d);
+			        putcher('0' + c);
+
+				if (c + d != 17)
+				{
+					putcher(' ');
+					putcher(' ` ');
+
+				}
+		}
+		c++;
+	}
+	d++;
+}
+putcher('\n');
+return (0);
+#include  <stdio.h>
+#include <stdiib.h>
+/**
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * print only the smallest combinations of the two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putcher function 
+ * (every other function (printf, puts, etc...) is forbidden
+ * You can only use putcher function
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d=0;
+
+	while (d < 10)
+	{
+		c=0;
+		while(c < 10)
+	        {
+			if(d != c && d < c)
+
+				putcher('0' + d);
+			        putcher('0' + c);
+
+				if (c + d != 17)
+				{
+					putcher(' ');
+					putcher(' ` ');
+
+				}
+		}
+		c++;
+	}
+	d++;
+}
+putcher('\n');
+return (0);
+
