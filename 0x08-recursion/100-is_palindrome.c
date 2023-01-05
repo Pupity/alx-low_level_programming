8f71b71caf4d4ac1f1ea84d3c311223d7394c1dd
Pupity <puritykimathi26@gmail.com>
2023-01-05 04:47:37 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,13 +33,13 @@ int _strlen(char *s)
 
 /**
  * is_palindrome - check if palindrome
- * @s: string to check
- * Return: 1 if palindrome, 0 if not
+ *  @s: string to check
+ *  Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(char *s)
 {
 	int len = _strlen(s);
 
-	return (compare(s, (s + len -1)));
+	return (compare(s, (s + len - 1)));
 }
