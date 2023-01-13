b3685065c59f9752e463aa888c38af45525d755d
Pupity <puritykimathi26@gmail.com>
2023-01-13 12:46:01 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,9 +5,9 @@
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
  * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ *  @new_size: new size of the new memory block
  *
- * Return: pointer to the newly allocated memory block
+ *   Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -20,33 +20,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0 && ptr)
 	{
-		if (new_size == 0 && ptr)
-		{
-			free(ptr);
-			return (NULL);
-		}
-
-		if (!ptr)
-			return (malloc(new_size));
-
-		ptr1 = malloc(new_size);
-		if (!ptr1)
-			return (NULL);
-
-		old_ptr = ptr;
-
-		if (new_size < old_size)
-		{
-			for (i = 0; i < new_size; i++)
-				ptr1[i] = old_ptr[i];
-		}
-
-		if (new_size > old_size)
-		{
-			for (i = 0; i < old_size; i++)
-				ptr1[i] = old_ptr[i];
-		}
-		
 		free(ptr);
-		return (ptr1);
+		return (NULL);
 	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
