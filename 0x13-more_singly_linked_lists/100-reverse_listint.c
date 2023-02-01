4a7a34e6a69970366b349c81324d064afa600b5d
Pupity <puritykimathi26@gmail.com>
2023-02-01 10:31:25 -0800

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,8 +4,25 @@
  * @head: describe argument
  * Return: what does it return?
  */
+
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev, *next;
+
+	/* account for no ptr and empty list */
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	/* iterate thorugh list to reverse linking */
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next; /* keep track of next node to move to */
+		(*head)->next = prev; /* link current node's ptr to prev node */
+		prev = *head; /* update previous node to be current node */
+		*head = next; /* move current node to next node */
+	}
+	(*head) = prev; /* unlink last element from null and point backwards */
+
+	return (*head);
 }
