bccd3541bd6da5c3fc1618c785bb6503937ac2f4
Pupity <puritykimathi26@gmail.com>
2023-03-07 10:13:01 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-
 /**
-   * main - Entry point
-   * Return: 0 on success
-   */
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
-	int last_number;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	l = n % 10;
 
-	/* your code goes there */
-	last_number = abs(n);
-
-	while (last_number > 10)
-        {
-		last_number %= 10;
-  }
-
-	printf("Last digit of %d is %d ", n, last_number);
-	if (last_number > 5)
-		puts("and is greater than 5");
-	else if (last_number == 0)
-		puts("and is 0");
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
 	else
-		puts("and is less than 6 and not 0");
-
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
 	return (0);
 }
-
